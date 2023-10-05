6e3f82eb0a161ad9d49cc019aa21ba35cbc019ee
adityapadmanair <adityapnair15@gmail.com>
2023-10-05 22:52:32 +0200

    Tried to consider there are more filesin 1 folder

--- src/main.js
+++ src/main.js
@@ -3,31 +3,33 @@ import { version } from '@bokeh/bokehjs';
 import axios from 'axios';
 import { NetCDFReader } from 'netcdfjs';
 
+// Define the base URL for NetCDF files
+const baseFolder = 'http://localhost:8000/test/example_files/synthetic_square/dzdata_files/';
+const maxZoomLevel = 9;
+const zoomLevel = 2;
 
-// URL of the NetCDF file
-    const netcdfUrl = 'http://localhost:8000/test/example_files/synthetic_square/dzdata_files/7/0_0.nc';
+if (zoomLevel >= 0 && zoomLevel <= maxZoomLevel) {
+  const zoomedFolderUrl = `${baseFolder}${zoomLevel}/`;
 
-    // Make a GET request using Axios
-    axios.get(netcdfUrl, { responseType: 'arraybuffer' })
-      .then((response) => response.data)
-      .then((data) => {
+  // Make a GET request using Axios to get a list of file names in the folder
+  axios.get(zoomedFolderUrl)
+    .then((response) => {
+      const fileNames = response.data; // Assuming the server returns a list of file names
+      console.log(fileNames);
+      // Iterate through the file names using a for loop
+      for (let i = 0; i < fileNames.length; i++) {
+        const fileName = fileNames[i];
+        
+        // Make a GET request to load each NetCDF file
+        axios.get(`${zoomedFolderUrl}${fileName}`, { responseType: 'arraybuffer' })
+          .then((response) => response.data)
+          .then((data) => {
             const netcdfReader = new NetCDFReader(data);
             const heights = netcdfReader.getDataVariable('heights');
 
-            //let allHeights = [];
-
-            //for (let i = 0; i < heights.length; i++) {
-            //  allHeights.push(heights[i]);
-            //  console.log(i)
-            
-            console.log(Palettes.viridis(20));
-          	console.log(heights);
-
+            // Create an HTML canvas element and visualize the data here
             const canvas = document.getElementById("myCanvas");
             const ctx = canvas.getContext("2d");
-
-            console.log(canvas.height);
-            console.log(canvas.width);
 
             const c_width = canvas.width;
             const c_height = canvas.height;
@@ -37,35 +39,16 @@ import { NetCDFReader } from 'netcdfjs';
 
             const xDim = dimensions.find(dim => dim.name === 'x');
             const yDim = dimensions.find(dim => dim.name === 'y');
-            //console.log(xDim);
             const ySize = yDim.size;
             const xSize = xDim.size;
-            //console.log(`x dimension size: ${xSize}`);
-
 
             console.log(ySize);
             console.log(xSize);
 
-
-            // greyscale without bokeh
-
-            //for (let y = 0; y < ySize; y++) {
-            //  for (let x = 0; x < xSize; x++) {
-            //    const value = heights[y * ySize + x];  
-            //    const grayscaleValue = Math.floor((value * 255)); // need to adjust based on data range
-            //    ctx.fillStyle = `rgb(${grayscaleValue}, ${grayscaleValue}, ${grayscaleValue})`;
-            //    ctx.fillRect(x, y, 1, 1);
-            //  }
-            //}
-
-
             // Bokeh color map
             const colorPalette = Palettes.inferno(20); 
-            // Defining a color scale function
             const minValue = Math.min(...heights);
             const maxValue = Math.max(...heights);
-
-            const imageData = ctx.createImageData(xSize, ySize);
 
             for (let y = 0; y < ySize; y++) {
               for (let x = 0; x < xSize; x++) {
@@ -74,20 +57,16 @@ import { NetCDFReader } from 'netcdfjs';
                 const colorIndex = Math.floor(scaledValue * (colorPalette.length - 1));
                 const color = colorPalette[colorIndex];
 
-
-                const htmlColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 1)`;
-                
-                // Calculate the pixel index in the ImageData data array
-                const pixelIndex = (y * xSize + x) * 4; // RGBA format
-
-                // Set the RGBA values in the ImageData
-                imageData.data[pixelIndex] = color[0];     // Red
-                imageData.data[pixelIndex + 1] = color[1]; // Green
-                imageData.data[pixelIndex + 2] = color[2]; // Blue
-                imageData.data[pixelIndex + 3] = 255;      // Alpha (fully opaque)
+                ctx.fillStyle = '#' + (color.toString(16).substring(0, 6));
+                ctx.fillRect(x, y, 1, 1);
               }
             }
-
-            // Draw the ImageData onto the canvas
-            ctx.putImageData(imageData, 0, 0);
           });
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching file list:', error);
+    });
+} else {
+  console.error('Invalid zoom level. Zoom level must be between 0 and 9.');
+}
