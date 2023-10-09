e15888b2b95018c2e458afc9c6e508686ebfea33
adityapadmanair <adityapnair15@gmail.com>
2023-10-09 12:18:05 +0200

    Including the logic to find current pos

--- src/main.js
+++ src/main.js
@@ -3,70 +3,79 @@ import { version } from '@bokeh/bokehjs';
 import axios from 'axios';
 import { NetCDFReader } from 'netcdfjs';
 
-// Define the base URL for NetCDF files
-const baseFolder = 'http://localhost:8000/test/example_files/synthetic_square/dzdata_files/';
-const maxZoomLevel = 9;
-const zoomLevel = 2;
+// URL of the NetCDF file
+const netcdfUrl = 'http://localhost:8000/test/example_files/synthetic_square/dzdata_files/7/0_0.nc';
 
-if (zoomLevel >= 0 && zoomLevel <= maxZoomLevel) {
-  const zoomedFolderUrl = `${baseFolder}${zoomLevel}/`;
+// Define the magnification level (Z)
+const Z = 0; // 
 
-  // Make a GET request using Axios to get a list of file names in the folder
-  axios.get(zoomedFolderUrl)
-    .then((response) => {
-      const fileNames = response.data; // Assuming the server returns a list of file names
-      console.log(fileNames);
-      // Iterate through the file names using a for loop
-      for (let i = 0; i < fileNames.length; i++) {
-        const fileName = fileNames[i];
-        
-        // Make a GET request to load each NetCDF file
-        axios.get(`${zoomedFolderUrl}${fileName}`, { responseType: 'arraybuffer' })
-          .then((response) => response.data)
-          .then((data) => {
-            const netcdfReader = new NetCDFReader(data);
-            const heights = netcdfReader.getDataVariable('heights');
+// Define the tile size (t)
+const t = 256; // doubt
 
-            // Create an HTML canvas element and visualize the data here
-            const canvas = document.getElementById("myCanvas");
-            const ctx = canvas.getContext("2d");
 
-            const c_width = canvas.width;
-            const c_height = canvas.height;
-            console.log(heights.length);
+// Select the canvas element from your HTML
+const canvas = document.getElementById('myCanvas');
 
-            const dimensions = netcdfReader.dimensions;
+// Define the canvas size (cx and cy)
+const cx = canvas.width; // You can change this value as needed
+const cy = canvas.height; // You can change this value as needed
 
-            const xDim = dimensions.find(dim => dim.name === 'x');
-            const yDim = dimensions.find(dim => dim.name === 'y');
-            const ySize = yDim.size;
-            const xSize = xDim.size;
+// Make a GET request using Axios
+axios.get(netcdfUrl, { responseType: 'arraybuffer' })
+  .then((response) => response.data)
+  .then((data) => {
+    const netcdfReader = new NetCDFReader(data);
+    const heights = netcdfReader.getDataVariable('heights');
 
-            console.log(ySize);
-            console.log(xSize);
+    const minValue = Math.min(...heights);
+    const maxValue = Math.max(...heights);
 
-            // Bokeh color map
-            const colorPalette = Palettes.inferno(20); 
-            const minValue = Math.min(...heights);
-            const maxValue = Math.max(...heights);
+    const colorPalette = Palettes.inferno(20);
 
-            for (let y = 0; y < ySize; y++) {
-              for (let x = 0; x < xSize; x++) {
-                const value = heights[y * xSize + x];
+    const dimensions = netcdfReader.dimensions;
+    const xDim = dimensions.find((dim) => dim.name === 'x');
+    const yDim = dimensions.find((dim) => dim.name === 'y');
+    const ySize = yDim.size;
+    const xSize = xDim.size;
+
+    const startX = 0; // Lower left canvas X pos
+    const startY = 0; // Lower left canvas Y pos
+
+    const effectiveXSize = xSize / (2 ** Z);
+    const effectiveYSize = ySize / (2 ** Z);
+
+    const tilesToRenderX = Math.ceil(cx / t);
+    const tilesToRenderY = Math.ceil(cy / t);
+
+    const ctx = canvas.getContext('2d');
+
+    for (let ty = 0; ty < tilesToRenderY; ty++) {
+      for (let tx = 0; tx < tilesToRenderX; tx++) {
+        const tileX = startX + tx * t;
+        const tileY = startY + ty * t;
+
+        if (tileX < effectiveXSize && tileY < effectiveYSize) {
+          for (let dy = 0; dy < t; dy++) {
+            for (let dx = 0; dx < t; dx++) {
+              const canvasX = tx * t + dx;
+              const canvasY = ty * t + dy;
+
+              const dataX = Math.floor(tileX + dx * (2 ** Z));
+              const dataY = Math.floor(tileY + dy * (2 ** Z));
+
+              if (dataX < xSize && dataY < ySize) {
+                const value = heights[dataY * xSize + dataX];
                 const scaledValue = (value - minValue) / (maxValue - minValue);
                 const colorIndex = Math.floor(scaledValue * (colorPalette.length - 1));
                 const color = colorPalette[colorIndex];
 
                 ctx.fillStyle = '#' + (color.toString(16).substring(0, 6));
-                ctx.fillRect(x, y, 1, 1);
+                ctx.fillRect(canvasX, canvasY, 1, 1);
               }
             }
-          });
+          }
+        }
       }
-    })
-    .catch((error) => {
-      console.error('Error fetching file list:', error);
-    });
-} else {
-  console.error('Invalid zoom level. Zoom level must be between 0 and 9.');
-}
+    }
+  });
+
