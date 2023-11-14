86f6d0301473e1d83824c2b52c47bb8956ecc97c
adityapadmanair <adityapnair15@gmail.com>
2023-11-14 16:55:26 +0100

    Code is working fine apart from small issues

--- src/main.js
+++ src/main.js
@@ -4,145 +4,141 @@ import axios from 'axios';
 import { NetCDFReader } from 'netcdfjs';
 
 // Define the magnification level (Z)
-let Z = 10; // 
+let Z = 9; // 
 
 // Initial visualization
  updateVisualization(Z);
+
+ function updateScaleBar(Z) {
+    const scaleValueElement = document.getElementById('scaleValue');
+    scaleValueElement.textContent = `Z = ${Z}`;
+  }
+
+function renderColorBar(colorPalette) {
+  const colorBarGradient = document.getElementById('colorBarGradient');
+  colorBarGradient.innerHTML = '';
+
+  colorPalette.forEach((color) => {
+    const colorBlock = document.createElement('div');
+    colorBlock.style.backgroundColor = `#${color.toString(16).substring(0, 6)}`;
+    colorBarGradient.appendChild(colorBlock);
+  });
+}
+
 
 // Loop through each NetCDF file URL
 function updateVisualization(Z) {
 
   // Define the constant part of the URL
-  const baseUrl = 'http://localhost:8000/test/example_files/synthetic_square/dzdata_files/';
+  const rootUrl = 'http://localhost:8000/test/example_files/synthetic_square/';
+  const baseUrl = `${rootUrl}dzdata_files/`;
 
-  // Define the fixed file names
-  const fileNames = [
-    '0_0.nc', '0_1.nc', '0_2.nc', '0_3.nc',
-    '1_0.nc', '1_1.nc', '1_2.nc', '1_3.nc',
-    '2_0.nc', '2_1.nc', '2_2.nc', '2_3.nc',
-    '3_0.nc', '3_1.nc', '3_2.nc', '3_3.nc'
-  ];
+  // Read JSON
+  axios.get(`${rootUrl}dzdata.json`).then(response => {
+    console.log(response.data);
+    console.log(response.data.Image.Size.Height);
 
-  // Create an array to store the generated URLs
-  const netcdfUrls = [];
+    // Extract data file size from metadata file
+    const imageSize = response.data.Image.Size;
+    const tileSize = response.data.Image.TileSize;
 
-  // Loop through each fixed file name and generate the URL based on Z
-  for (const fileName of fileNames) {
-    const url = `${baseUrl}${Z}/${fileName}`;
-    netcdfUrls.push(url);
-  }
+    // Compute effective data file size at current zoom level
+    const maxZoomLevel = Math.ceil(Math.log2(Math.max(imageSize.Width, imageSize.Height)));
+    const zoomLevelFactor = (2 ** (maxZoomLevel - Z + 1));
+    const widthAtZoomLevel = imageSize.Width / zoomLevelFactor;
+    const heightAtZoomLevel = imageSize.Height / zoomLevelFactor;
+    console.log(`width = ${widthAtZoomLevel}, height = ${heightAtZoomLevel}`);
 
-  // Select the canvas element from your HTML
-  const canvas = document.getElementById('myCanvas');
+    // Compute number of tiles at current zoom level
+    const numColumns = Math.ceil(widthAtZoomLevel / tileSize);
+    const numRows = Math.ceil(heightAtZoomLevel / tileSize);
 
-  // Define the canvas size (cx and cy)
-  const cx = canvas.width; // 
-  const cy = canvas.height; //
+    updateScaleBar(Z);
+    //renderColorBar();
 
+    // Select the canvas element from HTML
+    const canvas = document.getElementById('myCanvas');
 
-  // Define the number of rows and columns for the matrix
-  const numRows = 4;
-  const numCols = 4;
+    // Define the canvas size (cx and cy)
+    const cx = canvas.width; // 
+    const cy = canvas.height; //
 
-  // Create a variable to keep track of whether at least one file was successfully loaded
-  let atLeastOneFileLoaded = false;   //doubt
+    // Create a variable to keep track of whether at least one file was successfully loaded
+    let atLeastOneFileLoaded = false;   //doubt
 
-  // Loop through each NetCDF file URL
-    // Loop through each NetCDF file URL
-  netcdfUrls.forEach((netcdfUrl, index) => {
-    // Calculate the row and column indices based on the index in the fileNames array
-    const rowIndex = Math.floor(index / numCols);
-    const colIndex = index % numCols;
+    // We need the same color palette for all tiles;
+    const minValue = 0.0; //Math.min(...heights);
+    const maxValue = 1.0; //Math.max(...heights);
 
-    // Calculate the position for rendering in the canvas
-    const xPos = colIndex * (cx / numCols);
-    const yPos = rowIndex * (cy / numRows);
-    const cellWidth = cx / numCols;
-    const cellHeight = cy / numRows;
-    axios.get(netcdfUrl, { responseType: 'arraybuffer' })
-      .then((response) => response.data)
-      .then((data) => {
-        const netcdfReader = new NetCDFReader(data);
-        const heights = netcdfReader.getDataVariable('heights');
+    const colorPalette = Palettes.inferno(20);
 
-        const minValue = Math.min(...heights);
-        const maxValue = Math.max(...heights);
+    for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
+      for (let columnIndex = 0; columnIndex < numColumns; columnIndex++) {
+        const fileName = `${rowIndex}_${columnIndex}.nc`;
+        const netcdfUrl = `${baseUrl}${Z}/${fileName}`;
 
-        const colorPalette = Palettes.inferno(20);
+        // Calculate the position for rendering in the canvas
+        const xPos = columnIndex * (cx / numColumns);
+        const yPos = rowIndex * (cy / numRows);
+        const cellWidth = cx / numColumns;
+        const cellHeight = cy / numRows;
+        axios.get(netcdfUrl, { responseType: 'arraybuffer' })
+          .then((response) => response.data)
+          .then((data) => {
+            const netcdfReader = new NetCDFReader(data);
+            const heights = netcdfReader.getDataVariable('heights');
 
-        const ctx = canvas.getContext('2d');
-        //ctx.clearRect(0, 0, cx, cy);
+            const ctx = canvas.getContext('2d');
+            //ctx.clearRect(0, 0, cx, cy);
 
+            const dimensions = netcdfReader.dimensions;
+            const xDim = dimensions.find((dim) => dim.name === 'y');
+            const yDim = dimensions.find((dim) => dim.name === 'x');
+            const xSize = xDim.size;
+            const ySize = yDim.size;
 
+            console.log(xSize);
+            console.log(ySize);
 
-        const dimensions = netcdfReader.dimensions;
-        const xDim = dimensions.find((dim) => dim.name === 'x');
-        const yDim = dimensions.find((dim) => dim.name === 'y');
-        const ySize = yDim.size;
-        const xSize = xDim.size;
-
-        const overlapSize = 5;
-        const numFiles = fileNames.length;
-
-          // Identify the overlapping region
-        const overlapStart = index === 0 ? 0 : overlapSize;
-        const overlapEnd = index === numFiles - 1 ? heights.length : heights.length - overlapSize;
-
-
-        for (let y = 0; y < ySize; y++) {
+             for (let y = 0; y < ySize; y++) {
                 for (let x = 0; x < xSize; x++) {
-                  // Initialize the color variable
-                  let color;
+                  const value = heights[y * xSize + x];
+                  const scaledValue = (value - minValue) / (maxValue - minValue);
+                  const colorIndex = Math.floor(scaledValue * (colorPalette.length - 1));
+                  const color = colorPalette[colorIndex];
 
-          // Check if the current pixel is within the overlapping region
-          if (x >= overlapStart && x < overlapEnd) {
-            // Apply blending or smoothing in the overlapping region
-            const prevFileValue = heights[y * xSize + x - overlapSize];
-            const nextFileValue = heights[y * xSize + x + overlapSize];
 
-            // Calculate a weighted combination to blend the values smoothly
-            const blendFactor = (x - overlapStart) / (2 * overlapSize);
-            const blendedValue = (1 - blendFactor) * prevFileValue + blendFactor * nextFileValue;
-            const blendedValueScaled = (blendedValue - minValue) / (maxValue - minValue);
-            const colorIndex = Math.floor(blendedValueScaled * (colorPalette.length - 1));
-            color = colorPalette[colorIndex];
-          } else {
-            // Use the original value for non-overlapping regions
-            const value = heights[y * xSize + x];
-            const scaledValue = (value - minValue) / (maxValue - minValue);
-            const colorIndex = Math.floor(scaledValue * (colorPalette.length - 1));
-            color = colorPalette[colorIndex];
-          }
+                  const cellX = xPos + (x * cellWidth) / xSize;
+                  const cellY = yPos + (y * cellHeight) / ySize;
+                  const cellColor = '#' + (color.toString(16).substring(0, 6));
 
-          const cellX = xPos + (x * cellWidth) / xSize;
-          const cellY = yPos + (y * cellHeight) / ySize;
-          const cellColor = '#' + (color.toString(16).substring(0, 6));
+                  ctx.fillStyle = cellColor;
+                  ctx.fillRect(cellX, cellY, cellWidth / xSize, cellHeight / ySize);
+              }
+            }
 
-          ctx.fillStyle = cellColor;
-          ctx.fillRect(cellX, cellY, cellWidth / xSize, cellHeight / ySize);
+            // Set the flag to true to indicate that at least one file was successfully loaded
+            atLeastOneFileLoaded = true;
+          })
+          .catch((error) => {
+            console.error(`Error loading NetCDF file: ${netcdfUrl}`);
+            console.error(error);
+          });
         }
       }
 
-        // Set the flag to true to indicate that at least one file was successfully loaded
-        atLeastOneFileLoaded = true;
-      })
-      .catch((error) => {
-        console.error(`Error loading NetCDF file: ${netcdfUrl}`);
-        console.error(error);
-      });
+    // Check if at least one file was successfully loaded before rendering (not working properly)
+    if (atLeastOneFileLoaded) {
+      // Render the canvas or perform any additional actions
+      console.log('Successfully loaded');
+    } else {
+      // Handle the case where no files were successfully loaded (not working properly)
+      console.log('No files were successfully loaded');
+    }
+
+    // Display the current Z value in the console
+    console.log(`Current Z value: ${Z}`);
   });
-
-  // Check if at least one file was successfully loaded before rendering (not working properly)
-  if (atLeastOneFileLoaded) {
-    // Render the canvas or perform any additional actions
-    console.log('Successfully loaded');
-  } else {
-    // Handle the case where no files were successfully loaded (not working properly)
-    console.log('No files were successfully loaded');
-  }
-
-  // Display the current Z value in the console
-  console.log(`Current Z value: ${Z}`);
 }
 
 // Event listener for mouse scroll to control Z value
@@ -168,3 +164,4 @@ window.addEventListener('wheel', (event) => {
   updateVisualization(Z);
 });
 
+renderColorBar(Palettes.inferno(20));
